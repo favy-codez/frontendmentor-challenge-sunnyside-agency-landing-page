1aedfb76a6eaf07dc4e4689ac05efa44104ba0ad
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-15 13:22:23 +0100

    commit message

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
+
+
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const image = document.querySelector("hamImg");
 
 navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
+    // substitute for toggle
+//   console.log(links.classList);
+//   console.log(links.classList.contains("random"));
+//   console.log(links.classList.contains("links"));
+//   if (links.classList.contains("show-links")) {
+//     links.classList.remove("show-links");
+//   } else {
+//     links.classList.add("show-links");
+//   }
   links.classList.toggle("show-links");
 });
+
